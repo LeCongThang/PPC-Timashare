7de53ea9b2bf038cb199bd3af92e4cfef03b9d2d
QuangVinh <vinhhongquang@gmail.com>
2016-12-30 09:35:22 +0700

    Update Code

--- app/view/header.php
+++ app/view/header.php
@@ -6,8 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <base href="<?= BASE_DIR ?>">
     <link href="<?= BASE_DIR ?>css/style.css" rel="stylesheet" type="text/css">
-    <script type="text/javascript" src="<?= BASE_DIR ?>ckeditor/ckeditor.js"></script>
-    <script type="text/javascript" src="<?= BASE_DIR ?>js/main.js"></script>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" type="text/css" href="<?= BASE_DIR ?>css/style.css">
     <link rel="stylesheet" type="text/css" href="<?= BASE_DIR ?>css/responsive.css">
@@ -28,119 +26,15 @@
     <link rel="stylesheet" type="text/css" href="<?= BASE_DIR ?>css/stylehead.css">
     <script type="text/javascript"
             src="<?= BASE_DIR ?>bootstrapdatetimepicker/js/bootstrap-datetimepicker.min.js"></script>
+    <script type="text/javascript" src="<?= BASE_DIR ?>ckeditor/ckeditor.js"></script>
+    <script type="text/javascript" src="<?= BASE_DIR ?>js/main.js"></script>
     <link rel="stylesheet" href="<?= BASE_DIR ?>bootstrapdatetimepicker/css/bootstrap.min.css"/>
     <link rel="stylesheet" href="<?= BASE_DIR ?>bootstrapdatetimepicker/css/bootstrap.css"/>
     <script type="text/javascript" src="<?= BASE_DIR ?>bootstrapdatetimepicker/js/bootstrap-datetimepicker.min.js"/>
     <script type="text/javascript" src="<?= BASE_DIR ?>bootstrapdatetimepicker/js/bootstrap-datetimepicker.js"/>
     <script type="text/javascript" src="<?= BASE_DIR ?>bootstrapdatetimepicker/bootstrapv3/bootstrap/js/bootstrap.js"/>
     <script type="text/javascript">
-        $(document).ready(function () {
-            $('#btn_dangky').click(function () {
-                diaChiEmail = $('#diaChiEmail').val();
-                dienthoai = $('#dienthoai').val();
-                if (validateEmail(email)) {
-                } else {
-                    $("#thongbao").text(email + " không đúng");
-                    return false;
-                }
-                if (diaChiEmail == "" || dienthoai == "") {
-                    $('#thongbao').text("Hãy nhập đầy đủ thông tin");
-                    return false;
-                }
-                if (isNaN(dienthoai)) {
-                    $('#thongbao').text("Điện thoại phải là số");
-                    return false;
-                }
-                return true;
-            });
 
-            $('#btnLuuThongTin').click(function () {
-                tentaikhoan = $('#tentaikhoan').val();
-                diachitaikhoan = $('#diachitaikhoan').val();
-                sodienthoaitaikhoan = $('#sodienthoaitaikhoan').val();
-                loi = 0;
-                if (isNaN(sodienthoaitaikhoan)) {
-                    loi++;
-                    $('#thongbaoXemThongTin').text("Điện thoại phải là số");
-                }
-                if (tentaikhoan == "" || diachitaikhoan == "" || sodienthoaitaikhoan == ""
-                ) {
-                    loi++;
-                    $('#thongbaoXemThongTin').text("Hãy nhập đầy đủ thông tin");
-                }
-                if (loi != 0) {
-                    return false;
-                }
-                return true;
-            });
-
-            $('#btn_gui').click(function () {
-                tencongty = $('#ten').val();
-                dienthoaicongty = $('#dienthoaicongty').val();
-                emailcongty = $('#email').val();
-                loiguilh = 0;
-
-                if (tencongty == "" || dienthoaicongty == "" || emailcongty == "") {
-                    $('#thongbaoguilh').text("Hãy nhập đầy đủ thông tin liên hệ");
-                    loiguilh++;
-                }
-
-                if (loiguilh != 0) {
-                    return false;
-                }
-                return true;
-            });
-
-            $('#btnQuenMatKhau').click(function () {
-                tendangnhapll = $('#tendangnhapll').val();
-                sodienthoaitaikhoanll = $('#sodienthoaitaikhoanll').val();
-                loiguiqmk = 0;
-                if (isNaN(sodienthoaitaikhoanll)) {
-                    loiguiqmk++;
-                    $('#thongbaoQuenMatKhau').text("Điện thoại phải là số");
-                }
-                if (tendangnhapll == "" || sodienthoaitaikhoanll == "") {
-                    $('#thongbaoQuenMatKhau').text("Hãy nhập đầy đủ thông tin");
-                    loiguiqmk++;
-                }
-                if (loiguiqmk != 0) {
-                    return false;
-                }
-                return true;
-            });
-
-            $('#btn_dangnhap').click(function () {
-                tendangnhap = $('#username').val();
-                matkhau = $('#password').val();
-                if (tendangnhap == "" || matkhau == "") {
-                    $('#thongbaodn').text("Hãy nhập đầy đủ thông tin");
-                    return false;
-                }
-                return true;
-            });
-
-            $('#doimatkhau').click(function () {
-                matkhaucu = $('#matkhaucu').val();
-                matkhaumoi = $('#matkhaumoi').val();
-                nhaplaimatkhaumoi = $('#nhaplaimatkhaumoi').val();
-                loi = 0;
-                if (matkhaucu == "" || matkhaumoi == "") {
-                    loi++;
-                    $('#thongbaodoimatkhau').text("Hãy nhập đầy đủ thông tin");
-                }
-                else {
-                    if (matkhaumoi != nhaplaimatkhaumoi) {
-                        loi++;
-                        $('#thongbaodoimatkhau').text("Mật khẩu nhập lại không trùng khớp");
-                    }
-                }
-                if (loi != 0) {
-                    return false;
-                }
-                return true;
-            });
-
-        });
     </script>
 </head>
 <body>
