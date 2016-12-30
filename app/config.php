3f71f2c97c4df393cbdd5f4237bec0ed96d4c636
QuangVinh <vinhhongquang@gmail.com>
2016-12-30 14:24:33 +0700

    Update

---
+++ app/config.php
@@ -0,0 +1,13 @@
+<?php
+define("BASE_URL",'http://localhost:8080/PPC-Timeshare/');
+define("BASE_DIR", "/PPC-Timeshare/");
+
+define("HOST", "localhost");
+define('DB_NAME',  "ppctimeshare");
+define('USER_DB', "root");
+define('PASS_DB', "");
+
+define('DEFAULT_CONTROLLER','controller');
+define('DEFAULT_ACTION','index');
+
+define('NGONNGU','vi,en');
